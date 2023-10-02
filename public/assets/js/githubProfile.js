790ce7a4bb190302ab99b33d9bfffdd5fe1bc8f2
joaovic <joaovictorca2004@gmail.com>
2023-10-02 16:09:49 -0300

    página de formação completa

--- public/assets/js/githubProfile.js
+++ public/assets/js/githubProfile.js
@@ -8,20 +8,25 @@ const apiUrl = `https://api.github.com/users/${username}`;
 const nameElement = document.getElementById('name');
 const usernameElement = document.getElementById('username');
 const bioElement = document.getElementById('bio');
-const profileImageElement = document.getElementById('profile-image');
+const profileImageElement = document.querySelectorAll('.profile-image');
 
 // Faz a solicitação para a API do GitHub
 fetch(apiUrl)
   .then(response => response.json())
-  .then(({ name, login, bio, avatar_url }) => {
+  .then((data) => {
     // Extrai as informações necessárias
+    const { name, login, bio, avatar_url } = data;
 
-    // Atualiza os elementos HTML com as informações
+    // Atualiza os elementos HTML com as informações caso exista.
     nameElement ? nameElement.textContent = name : null;
     usernameElement ? usernameElement.textContent = `{${login}}` : null;
     bioElement ? bioElement.textContent = bio : null;
-    profileImageElement ? profileImageElement.src = avatar_url : null;
-    profileImageElement ? profileImageElement.alt = `Foto de Perfil de ${name}` : null;
+
+    profileImageElement ? profileImageElement.forEach(img => {
+      img.src = avatar_url;
+      img.alt = `Foto de Perfil do ${name}`;
+    }) : null;
+
   })
   .catch(error => {
     console.error('Erro ao obter informações do GitHub:', error);
